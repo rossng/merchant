e24106c33e313f9d83cc28a44a5e63f18146f86b
Ross Gardiner <ross-public@gmx.com>
2018-03-06 23:23:34 +0000

    Remove unused modules; migrate to actual set of primitives from SPJ paper

--- src/Declarative.hs
+++ src/Declarative.hs
@@ -14,64 +14,76 @@ import Data.GraphViz.Commands
 
 
 data ContractF next
-  = One Horizon
-  | Scale Int next
-  | ScaleObs next
-  | Timebound (Time, Time) next
+  = Zero
+  | One
   | Give next
-  | Or next next
   | And next next
+  | Or next next
+  | Truncate Time next
+  | Then next next
+  | Scale Observable next
+  | Get next
+  | Anytime next
   deriving (Functor)
 
 type Contract = Free ContractF
 
 type Time = Int
 type Scale = Double
-data Horizon = Time Time | Infinite deriving (Eq, Ord)
+data Horizon = Time Time | Infinite deriving (Eq, Ord, Show)
+data Observable = External String | Constant Int deriving (Show)
+
+zero' :: Contract a
+zero' = liftF Zero
 
 one' :: Contract a
-one' = liftF (One Infinite)
-
-scale' :: Int -> Contract a -> Contract a
-scale' n c = Free (Scale n c)
-
-scaleObs' :: Contract a -> Contract a
-scaleObs' c = Free (ScaleObs c)
-
-timebound' :: (Time, Time) -> Contract a -> Contract a
-timebound' t c = Free (Timebound t c)
+one' = liftF One
 
 give' :: Contract a -> Contract a
 give' c = Free (Give c)
 
-or' :: Contract () -> Contract () -> Contract ()
-or' c1 c2 = Free (Or c1 c2)
-
 and' :: Contract () -> Contract () -> Contract ()
 and' c1 c2 = Free (And c1 c2)
 
-getHorizon :: Contract a -> Horizon
-getHorizon (Pure a) = Infinite
-getHorizon (Free (One h)) = h
-getHorizon c = Infinite
+or' :: Contract () -> Contract () -> Contract ()
+or' c1 c2 = Free (Or c1 c2)
 
-setHorizon :: Horizon -> Contract a -> Contract a
-setHorizon h' (Free (One h)) = Free $ One h'
-setHorizon h' c = c
+truncate' :: Time -> Contract a -> Contract a
+truncate' t c = Free (Truncate t c)
 
-zcb :: Contract ()
-zcb = and' (give' (scale' 10 one')) (timebound' (0,1) (scale' 11 one'))
+then' :: Contract() -> Contract () -> Contract ()
+then' c1 c2 = Free (Then c1 c2)
+
+scale' :: Observable -> Contract a -> Contract a
+scale' n c = Free (Scale n c)
+
+scaleK' :: Int -> Contract a -> Contract a
+scaleK' n c = Free (Scale (Constant n) c)
+
+get' :: Contract a -> Contract a
+get' c = Free (Get c)
+
+anytime' :: Contract a -> Contract a
+anytime' c = Free (Anytime c)
+
+zcb :: Time -> Int -> Contract ()
+zcb t x = scaleK' x (get' (truncate' t (one')))
+
+zcbExample :: Contract ()
+zcbExample = zcb 1 10
 
 -- naive interpreter to text representation of contract
 interpret :: Contract a -> String
 interpret (Pure a) = ""
-interpret (Free (One h)) = "One"
-interpret (Free (Scale n cont)) = "Scale(" ++ show n ++ "," ++ interpret cont ++ ")"
-interpret (Free (ScaleObs cont)) = "ScaleObs(" ++ interpret cont ++ ")"
-interpret (Free (Timebound (t1,t2) cont)) = "Timebound(" ++ show t1 ++ "," ++ show t2 ++ "," ++ interpret cont ++ ")"
-interpret (Free (Give cont)) = "Give(" ++ interpret cont ++ ")"
-interpret (Free (Or c1 c2)) = "Or(" ++ interpret c1 ++ "," ++ interpret c2 ++ ")"
+interpret (Free Zero) = "Zero"
+interpret (Free One) = "One"
+interpret (Free (Give c)) = "Give(" ++ interpret c ++ ")"
 interpret (Free (And c1 c2)) = "And(" ++ interpret c1 ++ "," ++ interpret c2 ++ ")"
+interpret (Free (Or c1 c2)) = "Or(" ++ interpret c1 ++ "," ++ interpret c2 ++ ")"
+interpret (Free (Truncate t c)) = "Truncate(" ++ show t ++ "," ++ interpret c ++ ")"
+interpret (Free (Scale obs c)) = "ScaleObs(" ++ interpret c ++ ")"
+interpret (Free (Get c)) = "Get(" ++ interpret c ++ ")"
+interpret (Free (Anytime c)) = "Anytime(" ++ interpret c ++ ")"
 
 handle :: Functor f => (a -> b) -> (f b -> b) -> Free f a -> b
 handle gen alg (Pure x) = gen x
@@ -79,13 +91,16 @@ handle gen alg (Free x) = alg (fmap (handle gen alg) x)
 
 -- algebraic algebra to text representation of contract
 textAlg :: ContractF String -> String
-textAlg (One h) = "One"
-textAlg (Scale n cont) = "Scale(" ++ show n ++ "," ++ cont ++ ")"
-textAlg (ScaleObs cont) = "ScaleObs(" ++ cont ++ ")"
-textAlg (Timebound (t1,t2) cont) = "Timebound(" ++ show t1 ++ "," ++ show t2 ++ "," ++ cont ++ ")"
-textAlg (Give cont) = "Give(" ++ cont ++ ")"
-textAlg (Or c1 c2) = "Or(" ++ c1 ++ "," ++ c2 ++ ")"
+textAlg Zero = "Zero"
+textAlg One = "One"
+textAlg (Give c) = "Give(" ++ c ++ ")"
 textAlg (And c1 c2) = "And(" ++ c1 ++ "," ++ c2 ++ ")"
+textAlg (Or c1 c2) = "Or(" ++ c1 ++ "," ++ c2 ++ ")"
+textAlg (Truncate t c) = "Truncate(" ++ show t ++ "," ++ c ++ ")"
+textAlg (Then c1 c2) = "Then(" ++ c1 ++ "," ++ c2 ++ ")"
+textAlg (Scale n c) = "Scale(" ++ show n ++ "," ++ c ++ ")"
+textAlg (Get c) = "Get(" ++ c ++ ")"
+textAlg (Anytime c) = "Anytime(" ++ c ++ ")"
 
 prettyPrint :: Contract () -> String
 prettyPrint (Pure _) = ""
@@ -100,35 +115,46 @@ toGraph contract = digraph (Str "contract") (toDot [] contract)
 -- very naive interpreter for contracts to graphs using prefix list
 toDot :: [Int] -> Contract () -> Dot [Int]
 toDot prefix (Pure a) = node prefix [A.textLabel "()"]
-toDot prefix (Free (One h)) = node prefix [A.textLabel "One"]
-toDot prefix (Free (Scale n cont)) = do
-  node prefix [A.textLabel (pack ("Scale " ++ show n))]
-  toDot (0 : prefix) cont
-  prefix --> (0 : prefix)
-toDot prefix (Free (ScaleObs cont)) = do
-  node prefix [A.textLabel "ScaleObs"]
-  toDot (0 : prefix) cont
-  prefix --> (0 : prefix)
-toDot prefix (Free (Timebound (t1,t2) cont)) = do
-  node prefix [A.textLabel (pack ("Timebound " ++ show t1 ++ " " ++ show t2))]
-  toDot (0 : prefix) cont
-  prefix --> (0 : prefix)
-toDot prefix (Free (Give cont)) = do
+toDot prefix (Free Zero) = node prefix [A.textLabel "Zero"]
+toDot prefix (Free One) = node prefix [A.textLabel "One"]
+toDot prefix (Free (Give c)) = do
   node prefix [A.textLabel "Give"]
-  toDot (0 : prefix) cont
+  toDot (0 : prefix) c
   prefix --> (0 : prefix)
-toDot prefix (Free (Or c1 c2)) = do
-  node prefix [A.textLabel "Or"]
-  toDot (0 : prefix) c1
-  toDot (1 : prefix) c2
-  prefix --> (0 : prefix)
-  prefix --> (1 : prefix)
 toDot prefix (Free (And c1 c2)) = do
   node prefix [A.textLabel "And"]
   toDot (0 : prefix) c1
   toDot (1 : prefix) c2
   prefix --> (0 : prefix)
   prefix --> (1 : prefix)
+toDot prefix (Free (Or c1 c2)) = do
+  node prefix [A.textLabel "Or"]
+  toDot (0 : prefix) c1
+  toDot (1 : prefix) c2
+  prefix --> (0 : prefix)
+  prefix --> (1 : prefix)
+toDot prefix (Free (Truncate t c)) = do
+  node prefix [A.textLabel (pack ("Timebound " ++ show t))]
+  toDot (0 : prefix) c
+  prefix --> (0 : prefix)
+toDot prefix (Free (Then c1 c2)) = do
+  node prefix [A.textLabel "Then"]
+  toDot (0 : prefix) c1
+  toDot (1 : prefix) c2
+  prefix --> (0 : prefix)
+  prefix --> (1 : prefix)
+toDot prefix (Free (Scale n c)) = do
+  node prefix [A.textLabel (pack ("Scale " ++ show n))]
+  toDot (0 : prefix) c
+  prefix --> (0 : prefix)
+toDot prefix (Free (Get c)) = do
+  node prefix [A.textLabel "Get"]
+  toDot (0 : prefix) c
+  prefix --> (0 : prefix)
+toDot prefix (Free (Anytime c)) = do
+  node prefix [A.textLabel "Anytime"]
+  toDot (0 : prefix) c
+  prefix --> (0 : prefix)
 
 renderToGraph' :: Contract () -> FilePath -> IO FilePath
 renderToGraph' c fp = runGraphviz (toGraph' c) Png fp
@@ -147,37 +173,17 @@ toDot' :: Contract () -> StateT Int (DotM Int) ()
 toDot' (Pure a) = do
   n <- increment
   lift $ node n [A.textLabel "()"]
-toDot' (Free (One h)) = do
+toDot' (Free Zero) = do
+  n <- increment
+  lift $ node n [A.textLabel "Zero"]
+toDot' (Free One) = do
   n <- increment
   lift $ node n [A.textLabel "One"]
-toDot' (Free (Scale f cont)) = do
-  n <- increment
-  lift $ node n [A.textLabel (pack ("Scale " ++ show f))]
-  lift $ n --> (n + 1)
-  toDot' cont
-toDot' (Free (ScaleObs cont)) = do
-  n <- increment
-  lift $ node n [A.textLabel "ScaleObs"]
-  lift $ n --> (n + 1)
-  toDot' cont
-toDot' (Free (Timebound (t1,t2) cont)) = do
-  n <- increment
-  lift $ node n [A.textLabel (pack ("Timebound " ++ show t1 ++ " " ++ show t2))]
-  lift $ n --> (n + 1)
-  toDot' cont
 toDot' (Free (Give cont)) = do
   n <- increment
   lift $ node n [A.textLabel "Give"]
   lift $ n --> (n + 1)
   toDot' cont
-toDot' (Free (Or c1 c2)) = do
-  n <- increment
-  lift $ node n [A.textLabel "Or"]
-  lift $ n --> (n + 1)
-  toDot' c1
-  branch <- get
-  lift $ n --> branch
-  toDot' c2
 toDot' (Free (And c1 c2)) = do
   n <- increment
   lift $ node n [A.textLabel "And"]
@@ -186,46 +192,64 @@ toDot' (Free (And c1 c2)) = do
   branch <- get
   lift $ n --> branch
   toDot' c2
+toDot' (Free (Or c1 c2)) = do
+  n <- increment
+  lift $ node n [A.textLabel "Or"]
+  lift $ n --> (n + 1)
+  toDot' c1
+  branch <- get
+  lift $ n --> branch
+  toDot' c2
+toDot' (Free (Truncate t c)) = do
+  n <- increment
+  lift $ node n [A.textLabel (pack ("Truncate " ++ show t))]
+  lift $ n --> (n + 1)
+  toDot' c
+toDot' (Free (Then c1 c2)) = do
+  n <- increment
+  lift $ node n [A.textLabel "Then"]
+  lift $ n --> (n + 1)
+  toDot' c1
+  branch <- get
+  lift $ n --> branch
+  toDot' c2
+toDot' (Free (Scale f c)) = do
+  n <- increment
+  lift $ node n [A.textLabel (pack ("Scale " ++ show f))]
+  lift $ n --> (n + 1)
+  toDot' c
+toDot' (Free (Get c)) = do
+  n <- increment
+  lift $ node n [A.textLabel "Get"]
+  lift $ n --> (n + 1)
+  toDot' c
+toDot' (Free (Anytime c)) = do
+  n <- increment
+  lift $ node n [A.textLabel "Anytime"]
+  lift $ n --> (n + 1)
+  toDot' c
 
 renderToGraphAlg :: Contract () -> FilePath -> IO FilePath
-renderToGraphAlg c fp = runGraphviz (toGraphAlg c) Png fp
+renderToGraphAlg c = runGraphviz (toGraphAlg c) Png
 
+-- proper interpreter for contract -> graph rendering
 toGraphAlg :: Contract () -> G.DotGraph Int
 toGraphAlg contract = digraph (Str "contract") (evalStateT finalState 0)
   where finalState = handle pure dotAlg contract
 
+-- proper handler for contract -> graph rendering
 dotAlg :: ContractF (StateT Int (DotM Int) ()) -> StateT Int (DotM Int) ()
-dotAlg (One h) = do
+dotAlg Zero = do
+  n <- increment -- think about whether we actually want to increment at the leaves
+  lift $ node n [A.textLabel "Zero"]
+dotAlg One = do
   n <- increment -- think about whether we actually want to increment at the leaves
   lift $ node n [A.textLabel "One"]
-dotAlg (Scale f graph) = do
-  n <- increment
-  graph
-  lift $ node n [A.textLabel (pack ("Scale" ++ show f))]
-  lift $ n --> (n + 1)
-dotAlg (ScaleObs graph) = do
-  n <- increment
-  graph
-  lift $ node n [A.textLabel "ScaleObs"]
-  lift $ n --> (n + 1)
-dotAlg (Timebound (t1,t2) graph) = do
-  n <- increment
-  graph
-  lift $ node n [A.textLabel (pack ("Timebound " ++ show t1 ++ " " ++ show t2))]
-  lift $ n --> (n + 1)
 dotAlg (Give graph) = do
   n <- increment
   graph
   lift $ node n [A.textLabel "Give"]
   lift $ n --> (n + 1)
-dotAlg (Or graph1 graph2) = do
-  n <- increment
-  graph1
-  m <- get
-  graph2
-  lift $ node n [A.textLabel "Or"]
-  lift $ n --> (n + 1)
-  lift $ n --> m
 dotAlg (And graph1 graph2) = do
   n <- increment
   graph1
@@ -234,14 +258,54 @@ dotAlg (And graph1 graph2) = do
   lift $ node n [A.textLabel "And"]
   lift $ n --> (n + 1)
   lift $ n --> m
+dotAlg (Or graph1 graph2) = do
+  n <- increment
+  graph1
+  m <- get
+  graph2
+  lift $ node n [A.textLabel "Or"]
+  lift $ n --> (n + 1)
+  lift $ n --> m
+dotAlg (Truncate t graph) = do
+  n <- increment
+  graph
+  lift $ node n [A.textLabel (pack ("Truncate " ++ show t))]
+  lift $ n --> (n + 1)
+dotAlg (Then graph1 graph2) = do
+  n <- increment
+  graph1
+  m <- get
+  graph2
+  lift $ node n [A.textLabel "Then"]
+  lift $ n --> (n + 1)
+  lift $ n --> m
+dotAlg (Scale f graph) = do
+  n <- increment
+  graph
+  lift $ node n [A.textLabel (pack ("Scale" ++ show f))]
+  lift $ n --> (n + 1)
+dotAlg (Get graph) = do
+  n <- increment
+  graph
+  lift $ node n [A.textLabel "Get"]
+  lift $ n --> (n + 1)
+dotAlg (Anytime graph) = do
+  n <- increment
+  graph
+  lift $ node n [A.textLabel "Anytime"]
+  lift $ n --> (n + 1)
 
---data Balances = Balances { _party :: Double, _counterparty :: Double } deriving Show
---makeLenses ''Balances
---
---defaultBalances = Balances 0.0 0.0
---simpleContract = choose receiveOne' sendOne' `then'`
---  (do factor <- observe'
---      scale' factor
---      choose receiveOne' sendOne')
+horizon :: Contract () -> Horizon
+horizon c = handle (const Infinite) horizonAlg c
 
---executionResult = runStateT (interpret (1.0, 0) simpleContract) defaultBalances
+horizonAlg :: ContractF Horizon -> Horizon
+horizonAlg Zero = Infinite
+horizonAlg One = Infinite
+horizonAlg (Give c) = c
+horizonAlg (And c1 c2) = max c1 c2
+horizonAlg (Or c1 c2) = max c1 c2
+horizonAlg (Truncate t c) = min (Time t) c
+horizonAlg (Then c1 c2) = max c1 c2
+horizonAlg (Scale obs c) = c
+horizonAlg (Get c) = c
+horizonAlg (Anytime c) = c
